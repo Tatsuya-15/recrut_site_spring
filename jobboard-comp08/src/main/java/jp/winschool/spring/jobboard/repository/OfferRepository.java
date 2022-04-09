92ebc0de6679ae2e74ede4a33b3b169564a8749e
Tatsuya-15 <fiuto54gkar@outlook.com>
2022-04-09 21:20:14 +0900

    求人サイトサンプル

---
+++ jobboard-comp08/src/main/java/jp/winschool/spring/jobboard/repository/OfferRepository.java
@@ -0,0 +1,12 @@
+package jp.winschool.spring.jobboard.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jp.winschool.spring.jobboard.model.Offer;
+
+public interface OfferRepository extends JpaRepository<Offer, Integer> {
+    List<Offer> findByActiveTrue();
+    List<Offer> findByActiveTrueAndTitleContainsAndPrefectureContains(String word, String prefecture);
+}
